0a31c15c26e88241c60fe9fa2760bc9a11b27037
Joshua Buhain <jbuhain1@gmail.com>
2023-01-22 16:24:03 -0800

    removed unnecessary comments

--- csv-combiner.php
+++ csv-combiner.php
@@ -2,45 +2,39 @@
 <?php
 
 /**************************************
- * This PHP script takes one or more CSV files, combines them, 
- * adds/populate an additional column, then write them to stdout.
+ * This PHP script takes one or more CSV files, 
+ *   adds&populate an additional column to each, 
+ *      then combines all and writes this result to stdout.
  * 
- * PMG Programming Challenge - CSV Combiner 
- * Submitted by Joshua Buhain (JB) 
+ *     PMG Programming Challenge - CSV Combiner 
+ *   Submitted by Joshua Buhain (JB) 
  * https://github.com/jbuhain/PMGProgrammingChallenge
 ***************************************/
 
-// Check if at least 2 files are provided as arguments
-// JB: Could implement exception handling in the future like when file is not found
 if ($argc < 3) {
     echo "Not enough arguments!\nExample command:'$ ./csv-combiner.php ./path/to/file1.csv ./path/to/file2.csv'\n";
     exit;
 }
 
-// Iterate through argv arguments
 for ($i = 1; $i < $argc; $i++) {
     $file = $argv[$i];
 
-    // Parse CSV file into array
+    /* JB: Could implement exception handling in the future like 
+    throw exception if file is not found. */
     $fileData = array_map('str_getcsv', file($file));
 
-    // Iterate through each line of this CSV file 
     foreach ($fileData as $j => $data) {
         if ($j == 0) {
-            // If first line of file, add additional column 'filename'
             array_push($fileData[$j],"filename");
         } else {
-            // For non-header rows, insert value for 'filename' which is this file's basename
             array_push($fileData[$j], basename($file));
         }
     }  
 
-    // Remove the header row if not first file
     if ($i != 1) {
         array_shift($fileData);
     }
 
-    // Write file data to STDOUT
     foreach ($fileData as $line) {
         fputcsv(STDOUT, $line);
     }
