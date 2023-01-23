20cc1b75746d1bcd34bc1b47966e73eaf5043d18
Joshua Buhain <jbuhain1@gmail.com>
2023-01-22 16:46:05 -0800

    added 2gb file possible implementation

--- generatefixtures.py
+++ generatefixtures.py
@@ -5,6 +5,9 @@ import hashlib
 import os.path as path
 import random
 
+import pandas as pd
+
+
 DIR = path.abspath(path.dirname(__file__))
 FILES = {
     'clothing.csv': ('Blouses', 'Shirts', 'Tanks', 'Cardigans', 'Pants', 'Capris', '"Gingham" Shorts',),
@@ -23,13 +26,25 @@ def write_file(writer, length, categories):
 
 
 def main():
+    # For writing large > 2gb file for test cases mentioned. 
+    # Updates accessories.csv, clothing.csv, and household_cleaners.csv to 2gb files
+    # 
+    # for fn, categories in FILES.items():
+    #     with open(path.join(DIR, 'fixtures', fn), 'w', encoding='utf-8') as fh:
+    #         write_file(
+    #             csv.writer(fh, doublequote=False, escapechar='\\', quoting=csv.QUOTE_ALL),
+    #             1000000000,
+    #             categories
+    #         )
+
     for fn, categories in FILES.items():
         with open(path.join(DIR, 'fixtures', fn), 'w', encoding='utf-8') as fh:
             write_file(
                 csv.writer(fh, doublequote=False, escapechar='\\', quoting=csv.QUOTE_ALL),
-                random.randint(100, 1000),
+                random.randint(3, 15),
                 categories
             )
+   
 
 
 if __name__ == '__main__':
